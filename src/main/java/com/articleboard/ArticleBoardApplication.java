4aeaea3289675ebbe03c7350f29841987c80647b
kystic125 <kystic125@naver.com>
2026-01-03 17:28:30 +0900

    Initial commit

---
+++ src/main/java/com/articleboard/ArticleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.articleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/articleboard/ArticleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
