867f161a9474b21a3e16e67cda73d2898cc7a448
kystic125 <kystic125@naver.com>
2026-01-16 18:30:08 +0900

    feat: ArticleRepository 추가

---
+++ src/main/java/com/articleboard/article/repository/ArticleRepository.java
@@ -0,0 +1,23 @@
+package com.articleboard.article.repository;
+
+import com.articleboard.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Page<Article> findByIsNotice(boolean isNotice, Pageable pageable);
+
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+
+    Page<Article> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
+    Page<Article> findByWriterContaining(String writer, Pageable pageable);
+
+    Page<Article> findByLikeCountGreaterThanEqual(Long likeCount, Pageable pageable);
+}
